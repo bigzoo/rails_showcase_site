5876732245874b984b7be9ea8c78d276a8bfa641
Chris Nyaga <nyagachris75@gmail.com>
2017-08-04 11:36:42 +0300

    Contoller for the comments route created and updated with various CRUD methods.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,20 @@
 class CommentsController < ApplicationController
+  def new
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
+  end
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new(comment_params.merge({'post_id':@post.id}))
+    if @comment.save
+      redirect_to post_path(@comment.post)
+    else
+      @post = Post.find(params[:post_id])
+      render :new
+    end
+  end
+  private
+  def comment_params
+    params.require(:comment).permit(:user_id,:description)
+  end
 end
